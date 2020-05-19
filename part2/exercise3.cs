ecaeecc8d58aaa70fdbe305827df78838be6fc87
meikku <59999917+meikku@users.noreply.github.com>
2020-05-19 10:34:41 +0300

    ex 3 better version

---
+++ part2/exercise3.cs
@@ -0,0 +1,31 @@
+namespace part2
+{
+    using System;
+
+    public class Changes
+    {
+        public int Calculate(int[] t)
+        {
+            int changes = 0;
+           
+            for (int i = 1; i < t.Length; i++)
+            {
+                if (t[i - 1] == t[i])
+                {
+                    if (t[i] + 1 == t[i + 1])
+                    {
+                        t[i] = t[1] + 2;
+                    }
+                    else t[i]++;
+                    if (t[i] > 10)
+                    {
+                        t[i] = t[i] - 10;
+                    } 
+                    changes++;
+                }
+            }
+            return changes;
+        }
+    }
+}
+
