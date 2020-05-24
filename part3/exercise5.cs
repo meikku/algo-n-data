63442c6e85486086b16c258522603d4eb1cc31c6
meikku <59999917+meikku@users.noreply.github.com>
2020-05-24 18:41:34 +0300

    ex. 5 done

--- part3/exercise5.cs
+++ part3/exercise5.cs
@@ -6,25 +6,58 @@ namespace part3
 
         public int[] Create(int n, int k)
         {
-            int[] invers = new int[n];
 
+            int[] invers = new int[n];
+            
             for (int i = 0; i < n; i++)
             {
                 invers[i] = i + 1;
             }
-
-            int j = 0;
-            while (j < k)
+            int kMax = Sum(invers.Length - 1);
+            if (k > kMax)
             {
-                int swap = invers[j];
-                invers[j] = invers[j + 1];
-                invers[j + 1] = swap;
-                j = j + 2;
-                k++;
+                Console.WriteLine("Unable to do this many inversions. Doing " + kMax + " inversions instead. ");
+                k = kMax;
             }
 
-
+            int length = invers.Length;
+            while (k > 0)
+            {
+                for (int i = 1; i < length; i++)
+                {
+                    int swap = invers[i - 1];
+                    invers[i - 1] = invers[i];
+                    invers[i] = swap;
+                    k--;
+                    if (k == 0)
+                    {
+                        return invers;
+                    }
+                }
+                length--;
+            }
             return invers;
+
+            // public int[] Swap(int[] n, int k) // tried to do this with a recursive call, in vain
+            // {
+            //     for (int i = 1; i < n.Length; i++)
+            //     {
+            //         int swap = n[i - 1];
+            //         n[i - 1] = n[i];
+            //         n[i] = swap;
+            //         k--;
+            //         if (k == 0)
+            //         {
+            //             return n;
+            //         }
+            //     }
+            //     return n;
         }
+        static int Sum(int n)
+        {
+            if (n == 0) return 0;
+            else return Sum(n - 1) + n;
+        }
+
     }
 }
