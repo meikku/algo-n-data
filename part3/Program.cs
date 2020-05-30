db6c31847674aa961673cabf39867b75c6ab88c8
meikku <59999917+meikku@users.noreply.github.com>
2020-05-30 18:03:24 +0300

    more sort calls

--- part3/Program.cs
+++ part3/Program.cs
@@ -7,12 +7,39 @@ namespace part3
         static void Main(string[] args)
         {
             BinarySearch b = new BinarySearch();
-           // Console.WriteLine(b.Find(new int[] { 4, 1, 8, 5 }, 2)); // false
-           // Console.WriteLine(b.Find(new int[] { 0, 0 }, 0)); // true
-           Console.WriteLine(b.Find(new int[] { 4, 1, 8, 5, 8, 7, 4, 2, 3 }, 2)); // true
-           // Console.WriteLine(b.Find(new int[] { 0 }, 0)); // true
-           // Console.WriteLine(b.Find(Randomizer(100000), 3)); // depends on Random
+            Console.WriteLine(b.Find(new int[] { 4, 1, 8, 5 }, 2)); // false
+            Console.WriteLine(b.Find(new int[] { 0, 0 }, 0)); // true
+            Console.WriteLine(b.Find(new int[] { 4, 1, 8, 5, 8, 7, 4, 2, 3 }, 2)); // true
+            Console.WriteLine(b.Find(new int[] { 0 }, 0)); // true
+            Console.WriteLine(b.Find(Randomizer(100000), 3)); // depends on Random
+                                                              //   Sorting s = new Sorting();
+                                                              //   int[] sortMeTen = Randomizer(10);
+                                                              //   int[] sortMe = Randomizer(100);
+                                                              //   int[] sortMeThousand = Randomizer(1000);
+                                                              //   int[] sortMeLarger = Randomizer(10000);
+                                                              //   int[] sortMeLargerer = Randomizer(100000);
+                                                              //   int[] sortMeLargest = Randomizer(1000000);
+                                                              //   int[] sortMeMassive = Randomizer(10000000);
+
+            //   s.MergeSort(sortMeTen);
+            //   s.MergeSort(sortMe);
+            //   s.MergeSort(sortMeThousand);
+            //   s.MergeSort(sortMeLarger);
+            //   s.MergeSort(sortMeLargerer);
+            //   s.MergeSort(sortMeLargest);
+            //   s.MergeSort(sortMeMassive); // 
+
+            //   Console.WriteLine();
+
+            //   s.QuickSort(sortMeTen);
+            //   s.QuickSort(sortMe);
+            //   s.QuickSort(sortMeThousand);
+            //   s.QuickSort(sortMeLarger);
+            //   s.QuickSort(sortMeLargerer);
+            //   s.QuickSort(sortMeLargest);
+            //   s.QuickSort(sortMeMassive); 
         }
+
         public static int[] Randomizer(int n)
         {
             Random random = new Random();
@@ -26,3 +53,5 @@ namespace part3
         }
     }
 }
+
+
