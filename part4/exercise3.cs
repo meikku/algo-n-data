5bcfa8413060962dbb64252ffa513beb002051fb
meikku <59999917+meikku@users.noreply.github.com>
2020-05-31 18:41:29 +0300

    ex. 3 done

---
+++ part4/exercise3.cs
@@ -0,0 +1,39 @@
+namespace part4
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SmallestDistance
+    {
+        List<int> values = new List<int>();
+        public int difference;
+
+        public void Add(int x)
+        {
+            values.Add(x);
+        }
+
+        public int Calculate()
+        {
+            values.Sort();
+
+            // if (values.Count > 1) // WHY doesn't this work?
+            // {
+                int difference = values[1] - values[0];
+
+            // }
+
+            int thisDifference = 0;
+            for (int i = 1; i < values.Count; i++)
+            {
+                thisDifference = values[i] - values[i - 1];
+
+                if (thisDifference < difference)
+                {
+                    difference = thisDifference;
+                }
+            }
+            return difference;
+        }
+    }
+}
