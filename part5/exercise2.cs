5b5b02e412821aca511485cac8a766d7469c8e7b
meikku <59999917+meikku@users.noreply.github.com>
2020-06-05 13:21:29 +0300

    ex. 2 done

---
+++ part5/exercise2.cs
@@ -0,0 +1,49 @@
+namespace part5
+{
+    using System;
+    using System.Collections.Generic;
+    public class Connectivity
+    {
+        private List<int>[] graph;
+        private List<int> visited;
+
+        public Connectivity(int n)
+        {
+            this.graph = new List<int>[n + 1];
+
+            for (int i = 1; i <= n; i++)
+            {
+                graph[i] = new List<int>();
+            }
+            this.visited = new List<int>();
+        }
+        public void AddConnection(int a, int b)
+        {
+            graph[a].Add(b);
+            graph[b].Add(a);
+        }
+        public int Calculate(int x)
+        {
+            if (this.visited.Contains(x))
+            {
+                return 0;
+            }
+            this.visited.Add(x);
+
+            // Console.WriteLine("x is " + x);
+
+            // foreach (int number in this.visited)
+            // {
+            //     Console.WriteLine(number);
+            // }
+
+
+
+            foreach (int n in graph[x])
+            {
+                Calculate(n);
+            }
+            return this.visited.Count - 1;  
+        }
+    }
+}
